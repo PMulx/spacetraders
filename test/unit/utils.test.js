a82604ff63a60df61608240ff4917ea94cbe4654
PMulx <murail.anthonin@gmail.com>
2024-01-11 09:23:49 +0100

    test

--- test/unit/utils.test.js
+++ test/unit/utils.test.js
@@ -1,6 +1,10 @@
 import { expect, test } from "vitest";
 import { sum } from "../integration/sum";
-import { UserList } from "../../src/routes/UserList";
+// import { UserList } from "../../src/routes/index";
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+// test("User well connected", () => {
+//   expect;
+// });
