74a3a1bc644b28e05cc014f4965c98be9c9bf94e
PMulx <murail.anthonin@gmail.com>
2024-01-11 09:21:47 +0100

    trying

--- test/unit/utils.test.js
+++ test/unit/utils.test.js
@@ -1,5 +1,6 @@
 import { expect, test } from "vitest";
 import { sum } from "../integration/sum";
+import { UserList } from "../../src/routes/UserList";
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
 });
